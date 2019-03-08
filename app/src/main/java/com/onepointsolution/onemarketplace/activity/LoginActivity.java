051411516c5f5de7fab803abc40dba9eeeb11a3a
hemanth.gadi <hemanth.gadi@1pointsolution.com>
2019-03-08 17:40:40 +0530

    Added navigation-drawer to home screen

--- app/src/main/java/com/onepointsolution/onemarketplace/activity/LoginActivity.java
+++ app/src/main/java/com/onepointsolution/onemarketplace/activity/LoginActivity.java
@@ -32,7 +32,7 @@ public class LoginActivity extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
 
         if (auth.getCurrentUser() != null) {
-            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            startActivity(new Intent(LoginActivity.this, HomeActivity.class));
             finish();
         }
 

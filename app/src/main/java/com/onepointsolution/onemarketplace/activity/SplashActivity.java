4a0a9f7d7407b0d1a7366a1aafa65172c105dfaa
hemanth.gadi <hemanth.gadi@1pointsolution.com>
2019-03-22 12:03:42 +0530

    Added flash screen with logo.

---
+++ app/src/main/java/com/onepointsolution/onemarketplace/activity/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.onepointsolution.onemarketplace.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
